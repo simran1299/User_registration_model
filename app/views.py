833554d871394ccce058541cd60eddf9838cb65a
simran <simrandesai1299@gmail.com>
2023-01-04 21:46:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from app.forms import *
+
+from django.core.mail import send_mail
+def home(request):
+    return render(request,'home.html')
+
+
 def registration(request):
     uf=UserForm()
     pf=ProfileForm()
@@ -17,6 +23,14 @@ def registration(request):
             PFO=PD.save(commit=False)
             PFO.user=USO
             PFO.save()
+            send_mail('User registration',
+            'Registration is successfull',
+            '2kl18bm018@gmail.com',
+            [USO.email],fail_silently=False)
+
+
+
+
             return HttpResponse('Registration completed...')
 
     return render(request,'registration.html',d)
