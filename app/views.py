1daff30ec6da2b23a0d9475fd2a55964b837511b
simran <simrandesai1299@gmail.com>
2023-01-09 14:05:19 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 from app.forms import *
-
+from django.urls import reverse
 from django.core.mail import send_mail
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from app.models import *
+
+
+
+
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
 
 
@@ -34,3 +45,42 @@ def registration(request):
             return HttpResponse('Registration completed...')
 
     return render(request,'registration.html',d)
+
+def user_login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+
+
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile_info(request):
+    username=request.session.get('username')
+    USD=User.objects.get(username=username)
+    PFD=Profile.objects.get(user=USD)
+    d={'USD':USD,'PFD':PFD}
+
+    return render (request,'profile_info.html',d)
+
+@login_required
+def change_pw(request):
+    if request.method=='POST':
+        np=request.POST['np']
+        username=request.session['username']
+        USD=User.objects.get(username=username)
+        USD.set_password(np)
+        USD.save()
+        return HttpResponse('changed successfully')
+    return render(request,'change_pw.html')
+    
--- project32/urls.py
+++ project32/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
     path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
+    path('profile_info/',profile_info,name='profile_info'),
+    path('change_pw/',change_pw,name='change_pw'),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
